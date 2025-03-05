b67120fa98c4c71655183eff0be90f0d5532dbaf
HarryLee87 <harrylee@HARRYs-MacBook-Pro.local>
2025-03-05 16:31:16 -0700

    loading username

--- app.js
+++ app.js
@@ -3,15 +3,33 @@ const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
-function handleOnLoginSumit(event) {
+function handleOnLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem('username', username);
-  const usernnameCap = username.charAt(0).toUpperCase() + username.slice(1);
-  greeting.innerText = `Hello, ${usernnameCap}`;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function usernameCap(username) {
+  return username.charAt(0).toUpperCase() + username.slice(1);
+}
+
+function paintGreetings(username) {
+  const capitalizedUsername = usernameCap(username);
+  greeting.innerText = `Hello, ${capitalizedUsername}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', handleOnLoginSumit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', handleOnLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings(savedUsername);
+}
