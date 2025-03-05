1beeb51d2a696401c7bf6dde1795a8e0ba4ab109
HarryLee87 <harrylee@HARRYs-MacBook-Pro.local>
2025-03-05 16:11:38 -0700

    localstorage to store username

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert('hi');
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
+
+function handleOnLoginSumit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem('username', username);
+  const usernnameCap = username.charAt(0).toUpperCase() + username.slice(1);
+  greeting.innerText = `Hello, ${usernnameCap}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener('submit', handleOnLoginSumit);
