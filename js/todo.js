baf52e27d74187546a52847725a8d1665c02c3ff
HarryLee87 <harrylee@HARRYs-MacBook-Pro.local>
2025-03-05 21:06:02 -0700

    stored ToDos in localstorage and load them

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.querySelector('#todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.querySelector('#todo-list');
 
+const TODOS_KEY = 'todos';
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function paintToDo(newToDo) {
   const list = document.createElement('li');
   const span = document.createElement('span');
@@ -22,7 +30,17 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = '';
+  toDos.push(newToDo);
   paintToDo(newToDo);
+  saveToDos();
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach((todo) => paintToDo(todo));
+}
